1389d773007c10f82fa8c1a049dbac4e29ceb5b8
Lukas Larsson <garazdawi@gmail.com>
2013-06-11 07:40:37 +0200

    Optimize a bit

--- erts/emulator/beam/erl_term.h
+++ erts/emulator/beam/erl_term.h
@@ -20,6 +20,14 @@
 #ifndef __ERL_TERM_H
 #define __ERL_TERM_H
 
+/* Some tag ideas
+ * * tiny is 32 bit int on 64 bit. Only useable in list, only possible with 
+ *   tag in first 32 bit of word. For NaN encoding max tiny in 1 bsl 16 - 1. 
+ * * vlists
+ * * * maybe keep offset in bit 48+ on 64 bit?
+ * * lists as boxed
+ */
+
 #include "sys.h"
 
 #if defined(ARCH_64) && !HALFWORD_HEAP
