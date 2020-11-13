df0afff1cd1636532c8094fdf13bc1d2fbba79fc
Hans Bolinder <hasse@erlang.org>
2020-11-13 15:13:11 +0100

    compiler: Add option 'error_location'

--- lib/compiler/src/compile.erl
+++ lib/compiler/src/compile.erl
@@ -373,7 +373,13 @@ internal({forms,Forms}, Opts0) ->
     Source = proplists:get_value(source, Opts0, ""),
     Opts1 = proplists:delete(source, Opts0),
     Compile = build_compile(Opts1),
-    internal_comp(Ps, Forms, Source, "", Compile);
+    NewForms = case with_columns(Opts0) of
+                   true ->
+                       Forms;
+                   false ->
+                       strip_columns(Forms)
+               end,
+    internal_comp(Ps, NewForms, Source, "", Compile);
 internal({file,File}, Opts) ->
     {Ext,Ps} = passes(file, Opts),
     Compile = build_compile(Opts),
@@ -1004,12 +1010,18 @@ do_parse_module(DefEncoding, #compile{ifile=File,options=Opts,dir=Dir}=St) ->
                      true -> filename:basename(SourceName0);
                      false -> SourceName0
                  end,
+    StartLocation = case with_columns(Opts) of
+                        true ->
+                            {1,1};
+                        false ->
+                            1
+                    end,
     R = epp:parse_file(File,
                        [{includes,[".",Dir|inc_paths(Opts)]},
                         {source_name, SourceName},
                         {macros,pre_defs(Opts)},
                         {default_encoding,DefEncoding},
-                        {location,{1,1}},
+                        {location,StartLocation},
                         extra]),
     case R of
 	{ok,Forms,Extra} ->
@@ -1029,6 +1041,9 @@ do_parse_module(DefEncoding, #compile{ifile=File,options=Opts,dir=Dir}=St) ->
 	    Es = [{St#compile.ifile,[{none,?MODULE,{epp,E}}]}],
 	    {error,St#compile{errors=St#compile.errors ++ Es}}
     end.
+
+with_columns(Opts) ->
+    proplists:get_value(error_location, Opts, column) =:= column.
 
 find_invalid_unicode([H|T], File0) ->
     case H of
