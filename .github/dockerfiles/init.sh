2c735447eed4891337bd4cfba782cef5fc38bc06
Lukas Larsson <lukas@erlang.org>
2021-09-13 18:32:38 +0200

    Add testing to github actions

---
+++ .github/dockerfiles/init.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+sudo mkdir -p -m0755 /var/run/sshd
+
+sudo /usr/sbin/sshd
+
+sudo service postgresql start
+
+sudo -E bash -c "apt-get update && apt-get install -y linux-tools-common linux-tools-generic linux-tools-`uname -r`"
