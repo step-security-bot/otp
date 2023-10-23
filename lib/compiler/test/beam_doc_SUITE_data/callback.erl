d6f680bf12d91a3eaaec756c200b71745cb35251
Kiko Fernandez-Reyes <kiko@erlang.org>
2023-10-23 16:54:31 +0200

    compiler: fix test

--- lib/compiler/test/beam_doc_SUITE_data/callback.erl
+++ lib/compiler/test/beam_doc_SUITE_data/callback.erl
@@ -1,51 +1,61 @@
 -module(callback).
 
+-export([main/0]).
+
 %% -doc "
 %% This should be ignored
 %% ".
 %% -behaviour(gen_server).
 
--export([all_ok/0, main/0]).
+%% -export([all_ok/0, main/0]).
+
+-callback all_ok() -> ok.
 
 -doc "
 Callback fn that always returns ok.
 ".
--callback all_ok() -> ok.
-
 -doc "
-Test changing order
-".
--doc #{equiv => ok()}.
--callback change_order() -> Order :: boolean().
-
--callback param(X) -> X.
-
--callback ann(X :: integer) -> X :: integer().
-
-
--doc #{equiv => ok/0}.
--doc "
-This is a test
-".
-all_ok() ->
-    all_ok().
-
--doc #{equiv => main()}.
--spec main() -> ok.
--doc "
-all_ok()
-
-Calls all_ok/0
+Second callback
 ".
 main() ->
-    all_ok().
+     ok.
 
--doc #{equiv => main()}.
--doc "
-main2()
 
-Second main
-".
--spec main2() -> ok.
-main2() ->
-    ok.
+
+%% -doc "
+%% Test changing order
+%% ".
+%% -doc #{equiv => ok()}.
+%% -callback change_order() -> Order :: boolean().
+
+%% -callback param(X) -> X.
+
+%% -callback ann(X :: integer) -> X :: integer().
+
+
+%% -doc #{equiv => ok/0}.
+%% -doc "
+%% This is a test
+%% ".
+%% all_ok() ->
+%%     all_ok().
+
+%% -doc #{equiv => main()}.
+%% -spec main() -> ok.
+%% -doc "
+%% all_ok()
+
+%% Calls all_ok/0
+%% ".
+%% main() ->
+%%     all_ok().
+
+%% -doc #{equiv => main()}.
+%% -doc "
+%% main2()
+
+%% Second main
+%% ".
+%% -spec main2() -> ok.
+%% main2() ->
+%%     ok.
